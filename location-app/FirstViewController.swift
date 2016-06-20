66ed7dec46df8f64ddc21eea4e6cbf9702b3cddc
Amadeu Andrade <amadeuandrade@amadeus-macbook-pro.local>
2016-06-20 17:37:50 +0100

    Initial Commit

---
+++ location-app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  location-app
+//
+//  Created by Amadeu Andrade on 20/06/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
